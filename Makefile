571afbfb4552c197d50fb2609955a89e95ebef1b
hanhxiao <hanhxiao@tencent.com>
2019-09-25 11:50:05 +0800

    update analyzer

--- Makefile
+++ Makefile
@@ -3,8 +3,9 @@ build:
 	cd client && docker build --network=host -t gnes/benchmark:client . && cd -
 pull:
 	docker pull gnes/gnes:latest-alpine
-clean: ; docker stack rm my-gnes
-wait: ; @printf "sleep 15s for docker recycling network resources...\n"; sleep 15
-client: ; unset https_proxy && unset http_proxy && docker run --rm --network host gnes/benchmark:client --mode index
-deploy: ; docker stack deploy --compose-file docker-compose/network1.yml my-gnes
+clean: ; rm -rf .data && mkdir -p .data && docker stack rm my-gnes
+wait: ; @printf "sleep 10s for docker recycling network resources...\n" && sleep 10
+run_client: ; unset https_proxy && unset http_proxy && docker run --rm --network host -v $(PWD)/.data:/workspace gnes/benchmark:client --mode index --num_bytes $(s) --num_docs $(d) --batch_size $(b) --test_id $(id) --override_benchmark
+run_server: ; docker stack deploy --compose-file docker-compose/network$(id).yml my-gnes
+test: clean run_server run_client
 
--- client/app.py
+++ client/app.py
@@ -1,5 +1,11 @@
+from math import ceil
+
+import numpy as np
 from gnes.cli.parser import set_client_cli_parser
 from gnes.client.cli import CLIClient
+from gnes.helper import get_duration
+from gnes.proto import gnes_pb2
+from google.protobuf.json_format import Parse
 
 
 class MyClient(CLIClient):
@@ -9,6 +15,26 @@ class MyClient(CLIClient):
         for _ in range(self.args.num_docs):
             yield b'a' * self.args.num_bytes
 
+    def analyze(self):
+        num_effective_lines = ceil(self.args.num_docs / self.args.batch_size)
+        summary = {}
+        if self.args.test_id:
+            with open('/workspace/network%d.json' % self.args.test_id) as fp:
+                infos = [Parse(v, gnes_pb2.Envelope()).routes for v in fp.readlines()[-num_effective_lines:]]
+                summary['f:send interval'] = [get_duration(infos[j][0].start_time, infos[j + 1][0].start_time) for j in
+                                              range(len(infos) - 1)]
+                summary['f:recv interval'] = [get_duration(infos[j][0].end_time, infos[j + 1][0].end_time) for j in
+                                              range(len(infos) - 1)]
+                summary['f->r1 trans'] = [get_duration(j[0].start_time, j[1].start_time) for j in infos]
+                summary['r1->r2 trans'] = [get_duration(j[1].end_time, j[2].start_time) for j in infos]
+                summary['r2->f trans'] = [get_duration(j[2].end_time, j[0].end_time) for j in infos]
+                summary['roundtrip'] = [get_duration(j[0].start_time, j[0].end_time) for j in infos]
+
+        print('%40s\t%6s\t%6s\t%6s\t%6s\t%6s' % ('measure', 'mean', 'std', 'median', 'max', 'min'))
+        for k, v in summary.items():
+            print('%40s\t%3.3fs (+-%2.2f)\t%3.3fs\t%3.3fs\t%3.3fs' % (
+                k, np.mean(v), np.std(v), np.median(v), np.max(v), np.min(v)))
+
 
 if __name__ == '__main__':
     parser = set_client_cli_parser()
@@ -16,4 +42,10 @@ if __name__ == '__main__':
                         help='number of bytes per doc')
     parser.add_argument('--num_docs', type=int, default=10,
                         help='number of docs in total')
-    MyClient(parser.parse_args())
+    parser.add_argument('--test_id', type=int,
+                        help='test id')
+    parser.add_argument('--override_benchmark', action='store_true', default=False,
+                        help='override previous benchmark json data')
+    args = parser.parse_args()
+    m = MyClient(args)
+    m.analyze()
