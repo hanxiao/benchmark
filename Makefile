f485707e82cd8e93cc81b008fa40c6e44eac5f8d
hanhxiao <hanhxiao@tencent.com>
2019-09-26 18:20:09 +0800

    refactor test

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 all: clean build push
 build:
-	cd client && docker build --network=host -t gnes/benchmark:client . && cd -
+	cd client && docker build --network=host --build-arg GNES_IMG_TAG=$(GNES_IMG_TAG) -t gnes/benchmark:client . && cd -
 pull:
-	docker pull gnes/gnes:latest-alpine
-clean: ; rm -rf .data && mkdir -p .data && docker stack rm my-gnes
-wait: ; @printf "sleep 10s for docker recycling network resources...\n" && sleep 10
-run_client: ; unset https_proxy && unset http_proxy && docker run --rm --network host -v $(PWD)/.data:/workspace gnes/benchmark:client --mode index --num_bytes $(s) --num_docs $(d) --batch_size $(b) --test_id $(id) --override_benchmark
-run_server: ; docker stack deploy --compose-file docker-compose/network$(id).yml my-gnes
+	docker pull gnes/gnes:$(GNES_IMG_TAG)
+clean: ; rm -rf .data/network*.json && mkdir -p .data && docker stack rm my-gnes
+wait: ; @printf "sleep $(t)s for docker recycling network resources...\n" && sleep $(t)
+run_client: ; unset https_proxy && unset http_proxy && docker run --rm --network host --env GNES_BENCHMARK_ID=$(GNES_BENCHMARK_ID) --env GNES_IMG_TAG=$(GNES_IMG_TAG) -v $(PWD)/.data:/workspace gnes/benchmark:client --mode index --num_bytes $(s) --num_docs $(d) --batch_size $(b)
+run_server: ; docker stack deploy --compose-file docker-compose/network$(GNES_BENCHMARK_ID).yml my-gnes
 test: clean run_server run_client
 
--- client/app.py
+++ client/app.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+import os
 from math import ceil
 
 import numpy as np
@@ -18,22 +21,24 @@ class MyClient(CLIClient):
     def analyze(self):
         num_effective_lines = ceil(self.args.num_docs / self.args.batch_size)
         summary = {}
-        if self.args.test_id:
-            with open('/workspace/network%d.json' % self.args.test_id) as fp:
-                infos = [Parse(v, gnes_pb2.Envelope()).routes for v in fp.readlines()[-num_effective_lines:]]
-                summary['f:send interval'] = [get_duration(infos[j][0].start_time, infos[j + 1][0].start_time) for j in
-                                              range(len(infos) - 1)]
-                summary['f:recv interval'] = [get_duration(infos[j][0].end_time, infos[j + 1][0].end_time) for j in
-                                              range(len(infos) - 1)]
-                summary['f->r1 trans'] = [get_duration(j[0].start_time, j[1].start_time) for j in infos]
-                summary['r1->r2 trans'] = [get_duration(j[1].end_time, j[2].start_time) for j in infos]
-                summary['r2->f trans'] = [get_duration(j[2].end_time, j[0].end_time) for j in infos]
-                summary['roundtrip'] = [get_duration(j[0].start_time, j[0].end_time) for j in infos]
+        with open('/workspace/network.json') as fp:
+            infos = [Parse(v, gnes_pb2.Envelope()).routes for v in fp.readlines()[-num_effective_lines:]]
+            summary['f:send interval'] = [get_duration(infos[j][0].start_time, infos[j + 1][0].start_time) for j in
+                                          range(len(infos) - 1)]
+            summary['f:recv interval'] = [get_duration(infos[j][0].end_time, infos[j + 1][0].end_time) for j in
+                                          range(len(infos) - 1)]
+            summary['f->r1 trans'] = [get_duration(j[0].start_time, j[1].start_time) for j in infos]
+            summary['r1->r2 trans'] = [get_duration(j[1].end_time, j[2].start_time) for j in infos]
+            summary['r2->f trans'] = [get_duration(j[2].end_time, j[0].end_time) for j in infos]
+            summary['roundtrip'] = [get_duration(j[0].start_time, j[0].end_time) for j in infos]
 
         print('%40s\t%6s\t%6s\t%6s\t%6s\t%6s' % ('measure', 'mean', 'std', 'median', 'max', 'min'))
+
         for k, v in summary.items():
             print('%40s\t%3.3fs (+-%2.2f)\t%3.3fs\t%3.3fs\t%3.3fs' % (
                 k, np.mean(v), np.std(v), np.median(v), np.max(v), np.min(v)))
+
+        return {k: np.mean(v) for k, v in summary.items()}
 
 
 if __name__ == '__main__':
@@ -42,10 +47,24 @@ if __name__ == '__main__':
                         help='number of bytes per doc')
     parser.add_argument('--num_docs', type=int, default=10,
                         help='number of docs in total')
-    parser.add_argument('--test_id', type=int,
-                        help='test id')
-    parser.add_argument('--override_benchmark', action='store_true', default=False,
-                        help='override previous benchmark json data')
+    parser.add_argument('--retries', type=int, default=3,
+                        help='number of retries')
     args = parser.parse_args()
-    m = MyClient(args)
-    m.analyze()
+
+    all_summaries = []
+    for _ in range(args.retries):
+        m = MyClient(args)
+        all_summaries.append(m.analyze())
+
+    print('%40s\t%6s\t%6s\t%6s' % ('measure', 'best', 'worst', 'avg'))
+    final = {'version': os.environ.get('GNES_IMG_TAG', 'unknown'),
+             'timestamp': datetime.datetime.now().timestamp()}
+    for k in all_summaries[0].keys():
+        worst = np.max([j[k] for j in all_summaries])
+        best = np.min([j[k] for j in all_summaries])
+        avg = np.mean([j[k] for j in all_summaries])
+        print('%40s\t%3.3fs\t%3.3fs\t%3.3fs' % (k, best, worst, avg))
+        final[k] = float(best)
+
+    with open('/workspace/history%s.json' % os.environ.get('GNES_BENCHMARK_ID'), 'a', encoding='utf8') as fp:
+        fp.write(json.dumps(final, ensure_ascii=False, sort_keys=True) + '\n')
