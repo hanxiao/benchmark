148df8073600929e4720fb51998dc97d0006b9c8
hanhxiao <hanhxiao@tencent.com>
2019-09-27 15:39:45 +0800

    add run bash

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ for EXP_ID in 1 2 3 4 ;
 do
     export GNES_BENCHMARK_ID=$EXP_ID
     make pull && make build && make test d=1000 b=10 s=1000000 && make clean
-    make wait
+    make wait t=20
 done
