747800d6299ed35dfaed395f3a0b61b0bcf40aa7
hanhxiao <hanhxiao@tencent.com>
2019-09-27 15:33:30 +0800

    add run bash

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export GNES_IMG_TAG=latest-alpine
+
+for EXP_ID in 1 2 3 4 ;
+do
+    export GNES_BENCHMARK_ID=$EXP_ID
+    make pull && make build && make test d=1000 b=10 s=1000000 && make clean
+    make wait
+done
